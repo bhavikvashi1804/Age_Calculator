415ea958b18a81ca7b4062232d8871144c6ae7ff
Bhavik Vashi <bhavikvashi1804@gmail.com>
2020-04-11 12:22:31 +0530

    Logic for calculate month,day,year between cDate & Birthdate

--- lib/main.dart
+++ lib/main.dart
@@ -1,117 +1,138 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      title: 'Age Calculator',
+      theme: ThemeData.dark(),
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  DateTime _selectedDate;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+  void _presentDatePicker() {
+    showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime(1950),
+      lastDate: DateTime.now(),
+    ).then((pickedDate) {
+      if (pickedDate == null) {
+        return null;
+      } else {
+        setState(() {
+          _selectedDate = pickedDate;
+        });
+      }
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('Age Calculator'),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+            FlatButton.icon(
+              onPressed: () {
+                _presentDatePicker();
+              },
+              label: _selectedDate == null
+                  ? Text('Select Your Birth Date')
+                  : Text('Selected Date:  ' +
+                      DateFormat.yMMMMEEEEd().format(_selectedDate)),
+              icon: Icon(Icons.calendar_today),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            FlatButton(
+              onPressed: _selectedDate == null
+                  ? null
+                  : () {
+                      final cDate = DateTime.now();
+
+                      
+
+                      int day2=cDate.day, day1=_selectedDate.day, 
+                          mon2=cDate.month, mon1=_selectedDate.month, 
+                          year2=cDate.year, year1=_selectedDate.year;
+
+                      if (day2 < day1) {
+                        // borrow days from february
+                        if (mon2 == 3) {
+                          //  check whether year is a leap year
+                          if ((year2 % 4 == 0 && year2 % 100 != 0) ||
+                              (year2 % 400 == 0)) {
+                            day2 += 29;
+                          } else {
+                            day2 += 28;
+                          }
+                        }
+
+                        // borrow days from April or June or September or November
+                        else if (mon2 == 5 ||
+                            mon2 == 7 ||
+                            mon2 == 10 ||
+                            mon2 == 12) {
+                          day2 += 30;
+                        }
+
+                        // borrow days from Jan or Mar or May or July or Aug or Oct or Dec
+                        else {
+                          day2 += 31;
+                        }
+
+                        mon2 = mon2 - 1;
+                      }
+
+                      if (mon2 < mon1) {
+                        mon2 += 12;
+                        year2 -= 1;
+                      }
+
+                      int day_diff = day2 - day1;
+                      int mon_diff = mon2 - mon1;
+                      int year_diff = year2 - year1;
+
+                      print(day_diff);
+                      print(mon_diff);
+                      print(year_diff);
+                    },
+              child: Text('Calculate'),
+            ),
+            SizedBox(
+              height: 20,
             ),
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+              'Your Age is 20',
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
